7d4d0cb0640d22548864b8f82e8ba1ad599a45b2
aviraj26 <aviraj.j26@gmail.com>
2025-02-21 18:09:49 +0530

    files added

---
+++ basic_gates/design_source/and_gate.v
@@ -0,0 +1,6 @@
+module and_gate(
+    output c,
+    input a,b
+    );
+    and g1(c,a,b);
+endmodule
---
+++ basic_gates/design_source/nor_gate.v
@@ -0,0 +1,6 @@
+module nor_gate(
+    output c,
+    input a,b
+    );
+    nor g1(c,a,b);
+endmodule
